529291c79a7356927f7fe7d2840a6880d55980a1
minthantkyaw28 <68982186+minthantkyaw28@users.noreply.github.com>
2023-04-28 08:08:50 +0000

    HTML CSS and bootstrap added

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
 
 if __name__=="__main__":
   app.run("0.0.0.0",debug=True)
